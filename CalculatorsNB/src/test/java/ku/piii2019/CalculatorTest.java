56f32653da16d786ce7df0de1a36e5cdbc1d8b08
k1720552 <k1720552@PPRSB2022-I5.kuds.kingston.ac.uk>
2019-10-01 12:34:55 +0100

    Question 4. Altered getPercentage method to float to test it.

---
+++ CalculatorsNB/src/test/java/ku/piii2019/CalculatorTest.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ku.piii2019;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author k1720552
+ */
+public class CalculatorTest {
+    
+    public CalculatorTest() {
+    }
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+    
+    /*@BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }*/
+
+    /**
+     * Test of add method, of class Calculator.
+     */
+    @org.junit.jupiter.api.Test
+    public void testAdd() {
+        System.out.println("add");
+        int a = -4;
+        int b = 7;
+        Calculator instance = new BrokenAddCalculator();
+        int expResult = 3;
+        int result = instance.add(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of divide method, of class Calculator.
+     */
+    @org.junit.jupiter.api.Test
+    public void testDivide() {
+        System.out.println("divide");
+        int num = 8;
+        int denom = 2;
+        Calculator instance = new WorkingCalculator();
+        int expResult = 4;
+        int result = instance.divide(num, denom);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPercentage method, of class Calculator.
+     */
+    @org.junit.jupiter.api.Test
+    public void testGetPercentage() {
+        System.out.println("getPercentage");
+        float amount = 4.0F;
+        float total = 10.0F;
+        Calculator instance = new BrokenPercentageCalculator();
+        int expResult = 40;
+        float result = instance.getPercentage(amount, total);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    
+    /*public class BrokenPercentageCalculator implements Calculator {
+
+        public int add(int a, int b) {
+            return a+b;
+        }
+
+        public int divide(int num, int denom) {
+            return num/denom;
+        }
+
+        public int getPercentage(float amount, float total) {
+            return (int)((amount*101)/total);   //deliberate error
+        }
+    }*/
+    
+    
+}
